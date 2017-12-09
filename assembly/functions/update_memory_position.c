c37a5bffa7e60e39297db6a62cb4f6656c290d94
Davi Morales <davimmorales@gmail.com>
2017-12-09 21:10:18 -0200

    update_memory_position is working

--- assembly/functions/update_memory_position.c
+++ assembly/functions/update_memory_position.c
@@ -1,18 +1,24 @@
-function update_memory_position(id, former_index_process, new_index_process){
+function update_memory_position(id, new_index_process){
   i = 0;
+  former_index_process = 0;
+  new_index_process_aux;
   while (list_programs_info[i]!=0) {
     if(list_programs_info[i].id==id){
-      list_programs_info[i].index_process = new_position;
+      former_index_process = list_programs_info[i].index_process;
+      list_programs_info[i].index_process = new_index_process;
       break;
     }
     i++;
   }
 
   i=0;
-  former_position = former_position*size_of_section;
-  new_position = new_position*size_of_section;
+  former_index_process = former_index_process*size_of_section;
+  new_index_process_aux = new_index_process*size_of_section;
   while (i<size_of_section) {
-    running_processes[new_position+i] = running_processes[former_position+i];
+    running_processes[new_index_process_aux+i] = running_processes[former_index_process+i];
     i++;
   }
 }
+
+5 id
+6 new_index_process
