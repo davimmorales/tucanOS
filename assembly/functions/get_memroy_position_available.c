5730d235e4c7191691bea88655173c133cfd9b5a
Davi Morales <davimmorales@gmail.com>
2017-12-11 09:38:28 -0200

    get_memory_position_available was adapted to handle more processes

--- assembly/functions/get_memroy_position_available.c
+++ assembly/functions/get_memroy_position_available.c
@@ -1,21 +1,27 @@
 function get_memory_position_available(){
+  i = 0;
+  int list_running_map[9];
+  while(i<10){
+    list_running_map[i] = 0;
+    i++;
+  }
+
   int i = 0;
   int position;
   while(list_programs_info[i]!=0){
     if (list_programs_info[i].id != 1) {
       if (list_programs_info[i].state==1) {
-        if (list_programs_info[i].place==0) {
-          if(list_programs_info[i].index_process==1){
-            position = 2;
-            break;
-          }if(list_programs_info[i].index_process==2){
-            position = 1;
-            break;
-          }
-        }
+        list_running_map[list_programs_info[i].index_process] = 1;
       }
     }
     i++;
   }
-  return position;
+
+  i = 0;
+  while(i<10){
+    if(list_running_hd_map[i]==0){
+      position_available = i;
+      break;
+    }
+  }
 }
