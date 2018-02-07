c3405e73183223567825bfcbb64aae38474c663e
Davi Morales <davimmorales@gmail.com>
2018-02-07 21:31:03 -0200

    test c was written and some testing was made but didnt find bugs yet

---
+++ source/tests/C/phase_four_test_C.c
@@ -0,0 +1,13 @@
+function phase_four_test_B(){//circle area
+  int pi;
+  int radius;
+  int result;
+
+  pi = 3142;
+  input(radius);
+
+  result = pi*radius*radius;
+  result = result/1000;
+
+  output(result);
+}
